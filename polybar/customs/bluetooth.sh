3a842f095e475d9df46ce29cf84b3c449ae2e952
roistaff <roistaff1983@gmail.com>
2025-07-11 23:04:50 +0900

    add

--- polybar/customs/bluetooth.sh
+++ polybar/customs/bluetooth.sh
@@ -1,6 +1,6 @@
 if bluetoothctl show | grep -q "Powered: yes" ; then
 	if bluetoothctl info | grep -q "Connected: yes" ; then
-		echo "%{F#0000FF}%{F-}"
+		echo "%{F#0000FF}%{F-}"
 	else
 		echo "%{F#808080}%{F-}"
 	fi
